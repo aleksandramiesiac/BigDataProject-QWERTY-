ee783d92a6a0e5220b90556a27cfe06b5456330a
Agata Paldyna <paldyna.agata@gmail.com>
2019-05-06 01:11:59 +0200

    Dostosowanie pliku main.py do instancji na google cloud

--- caly_scraper/main.py
+++ caly_scraper/main.py
@@ -7,6 +7,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from scraping_data import ScrapData
+from pyvirtualdisplay import Display
 import itertools
 import os
 import datetime
@@ -14,7 +15,8 @@ import datetime
 
 def main():
 
-    os.environ["PATH"] += os.pathsep + os.getcwd()
+    main_directory = os.path.dirname(os.path.abspath(__file__))
+    os.environ["PATH"] += os.pathsep + main_directory
 
     # config = {}
     # with open("configuration_file.txt", "r") as conf_file:
@@ -23,7 +25,7 @@ def main():
     #         config[name.strip()] = val.replace("\n", "")
 
 
-    country_list = []#'Austria', 'Australia','Albania','Algeria','Brazil','Bulgaria','Belgium','Canada','China','Cuba','Cyprus','Croatia','Czech Republic','Denmark','Egypt','England','Estonia','Finland','France','Germany','Georgia','Greece','Hungary','Iceland','Ireland','Israel','Italy','Lithuania','Lativa','Malta','Mexico','Netherlands','New Zeland','Norway','Poland','Portugal','Russia','Romania','Saudi Arabia','Scotland','Spain','Singapore','Sweden','South Korea','Switzerland','Tunisia','Turkey','Thailand','Ukraine','United States']
+    country_list = ['Austria','Australia','Albania','Algeria','Brazil','Bulgaria','Belgium','Canada','China','Cuba','Cyprus','Croatia','Czech Republic','Denmark','Egypt','England','Estonia','Finland','France','Germany','Georgia','Greece','Hungary','Iceland','Ireland','Israel','Italy','Lithuania','Lativa','Malta','Mexico','Netherlands','New Zeland','Norway','Poland','Portugal','Russia','Romania','Saudi Arabia','Scotland','Spain','Singapore','Sweden','South Korea','Switzerland','Tunisia','Turkey','Thailand','Ukraine','United States']
 
     connections_list = list(itertools.product(country_list, repeat=2))
 
@@ -31,13 +33,16 @@ def main():
 
     print("------Uruchomienie programu--------\n" + currentDT.strftime("%Y-%m-%d_%H:%M"))
 
-    catalog = "flight_prices/"
+    catalog = main_directory + "/flight_prices/"
 
     if not os.path.exists(catalog):
         os.makedirs(catalog)
 
     file_path = catalog + currentDT.strftime("%Y-%m-%d") + ".txt"
     file_exists = os.path.exists(file_path)
+
+    display = Display(visible=0, size=(800, 600))
+    display.start()
 
     with open(file_path, "a") as output_file:
         if not file_exists:
@@ -46,7 +51,7 @@ def main():
 
         print("------Nazwa pliku--------\n" + output_file.name)
 
-        print("------Lista scrapowanych połączeń--------")
+        print("------Lista scrapowanych poĹ‚Ä…czeĹ„--------")
         print(connections_list)
 
         print("\n\n")
@@ -67,54 +72,53 @@ def choose_settings(country_from, country_to):
     options.add_argument('--headless')
 
     # Using Chrome to access web
-    browser = webdriver.Firefox(options=options)
+    with webdriver.Firefox(options=options) as browser:
+        website = 'http://www.azair.eu'
 
-    website = 'http://www.azair.eu'
+        # Open the website
+        browser.get(website)
 
-    # Open the website
-    browser.get(website)
+        from_box = browser.find_element_by_name('srcAirport')
 
-    from_box = browser.find_element_by_name('srcAirport')
+        browser.execute_script("arguments[0].value = ''", from_box)
+        from_box.send_keys(Keys.ARROW_DOWN)
+        browser.execute_script("arguments[0].value = ''", from_box)
 
-    browser.execute_script("arguments[0].value = ''", from_box)
-    from_box.send_keys(Keys.ARROW_DOWN)
-    browser.execute_script("arguments[0].value = ''", from_box)
+        from_box.send_keys(country_from)
+        from_box.send_keys(Keys.ARROW_DOWN)
+        from_box.send_keys(Keys.RETURN)
 
-    from_box.send_keys(country_from)
-    from_box.send_keys(Keys.ARROW_DOWN)
-    from_box.send_keys(Keys.RETURN)
+        to_box = browser.find_element_by_name('dstAirport')
 
-    to_box = browser.find_element_by_name('dstAirport')
+        browser.execute_script("arguments[0].value = ''", to_box)
+        to_box.send_keys(Keys.ARROW_DOWN)
+        browser.execute_script("arguments[0].value = ''", to_box)
 
-    browser.execute_script("arguments[0].value = ''", to_box)
-    to_box.send_keys(Keys.ARROW_DOWN)
-    browser.execute_script("arguments[0].value = ''", to_box)
+        to_box.send_keys(country_to)
+        to_box.send_keys(Keys.ARROW_DOWN)
+        to_box.send_keys(Keys.RETURN)
 
-    to_box.send_keys(country_to)
-    to_box.send_keys(Keys.ARROW_DOWN)
-    to_box.send_keys(Keys.RETURN)
+        # defaultowa waluta to EUR
+        #currency = browser.find_element_by_name('currency')
+        #currency.send_keys("PLN")
 
-    # defaultowa waluta to EUR
-    #currency = browser.find_element_by_name('currency')
-    #currency.send_keys("PLN")
+        old_url = browser.current_url
 
-    old_url = browser.current_url
+        search = browser.find_element_by_name('indexSubmit')
+        search.click()
 
-    search = browser.find_element_by_name('indexSubmit')
-    search.click()
+        try:
+	        WebDriverWait(browser, 60).until(EC.url_changes(old_url))
 
-    try:
-        WebDriverWait(browser, 60).until(EC.url_changes(old_url))
+	        if check_exists_by_xpath(browser,'@value="Repeat query" and @name="resultSubmit"'):
+		        raise TimeoutException
 
-        if check_exists_by_xpath(browser,'@value="Repeat query" and @name="resultSubmit"'):
-            raise TimeoutException
+        except TimeoutException:
+	        print("Timed out waiting for page to load")
 
-    except TimeoutException:
-        print("Timed out waiting for page to load")
+        print(browser.current_url)
 
-    print(browser.current_url)
-
-    return browser.current_url
+        return browser.current_url
 
 
 def check_exists_by_xpath(driver, xpath):
